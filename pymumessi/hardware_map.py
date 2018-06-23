4ff7dff88f058c04b09204fa2656ab11fc7e6164
Adam Anderson <adama@fnal.gov>
2018-06-23 17:56:47 -0500

    Add docstring to hardware_map.build()

--- pymumessi/hardware_map.py
+++ pymumessi/hardware_map.py
@@ -6,12 +6,29 @@
 # manipulating and checking hardware map DataFrames.
 
 import pandas as pd
+import numpy as np
 
-mandatory_fields = ['frequency', 'board number']
+mandatory_fields = ['board', 'channel_index', 'frequency', 'attenuation']
 
 def build(resonator_files):
+    '''
+    Constructs a hardware map pandas DataFrame from a list of filenames. Files
+    are assumed to be tab-separated text files containing the fields given in
+    'mandatory_fields'.
+
+    Parameters
+    ----------
+    resonator_files : list of str
+        List of filenames containing resonator channel information
+
+    Outputs
+    -------
+    hwm : pandas DataFrame
+        Hardware map
+    '''
+
     hwm = pd.DataFrame()
-    for fname in np.asarray(resonator_files):
+    for fname in resonator_files:
         board_hwm = pd.read_csv(fname, sep='\t')
         if any([field not in board_hwm.columns for field in mandatory_fields]):
             raise AttributeError('Mandatory field {} not in hardware map '
